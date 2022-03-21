5b1254b6c3fef7f0e6ee076eaae8af8ac18a91af
Luis Leon <luis.leon@fishtownanalytics.com>
2022-03-21 17:48:45 +0000

    adding additional fields for reporting filters

--- models/marts/core/fct_customer_orders.sql
+++ models/marts/core/fct_customer_orders.sql
@@ -21,7 +21,8 @@ final as (
         customer.name,
         customer.region_key,
         orders.net_item_sales_amount,
-        orders.order_date
+        orders.order_date,
+        0
 
     from
         orders
