4eb5fec858b4c6669f15f0e8b2e1b843c21dcd82
Luis Leon <luis.leon@fishtownanalytics.com>
2022-04-14 14:33:54 +0000

    testing sql linting

--- models/staging/tpch/stg_tpch_customers.sql
+++ models/staging/tpch/stg_tpch_customers.sql
@@ -9,7 +9,7 @@ renamed as (
     select
     
         c_custkey as customer_key,
-        'string as broken,
+        'string' as broken,
         c_name as name,
         c_address as address, 
         c_nationkey as nation_key,
