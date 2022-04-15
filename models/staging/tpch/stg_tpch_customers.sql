7538b7c85988676364fc9933269a7239019ae2ec
Luis Leon <luis.leon@fishtownanalytics.com>
2022-04-15 14:44:32 +0000

    Changing models and adding additional columns

--- models/staging/tpch/stg_tpch_customers.sql
+++ models/staging/tpch/stg_tpch_customers.sql
@@ -8,18 +8,15 @@ renamed as (
 
     select
     
-c_custkey as customer_key,
-         'literal' as bad, 
+        c_custkey as customer_key,
         c_name as name,
-       
         c_address as address, 
         c_nationkey as nation_key,  
         c_phone as phone_number,
-
-        
         c_acctbal as account_balance,
         c_mktsegment as market_segment,
-        c_comment as comment
+        c_comment as comment,
+        0 as id
 
     from source
 
