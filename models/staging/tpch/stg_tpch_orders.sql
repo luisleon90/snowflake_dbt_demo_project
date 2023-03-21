3c4c8904a141f03e3e162d84086c6f2edd6f4c9a
Luis Leon <luis.leon@fishtownanalytics.com>
2023-03-21 15:32:11 +0000

    removing unneeded columns

--- models/staging/tpch/stg_tpch_orders.sql
+++ models/staging/tpch/stg_tpch_orders.sql
@@ -16,10 +16,7 @@ renamed as (
         o_orderpriority as priority_code,
         o_clerk as clerk_name,
         o_shippriority as ship_priority,
-        o_comment as comment,
-        0,
-        1
-
+        o_comment as comment
     from source
 
 )
